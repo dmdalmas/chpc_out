0a42ebd568ecc30f06551800d712bc2e9ea889e3
dmdalmas <dmdalmasisu@gmail.com>
2023-03-29 13:49:13 -0600

    Laptop update

--- patfrq.r
+++ patfrq.r
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(stringr)
-df <- read.table("data_quechua.opf", header=F)
-names(df) <- c("SitePat", "I", "low", "high")
+df <- read.table("data_quechua_rma.opf", header=T)
+names(df) <- c("SitePat", "I")
 df$SitePat <- sapply(df$SitePat, str_replace_all, ":", "")
 
 # We want the site patterns printed in the order they appear in the file,
@@ -12,20 +12,18 @@ df$SitePat <- ordered(df$SitePat, levels=rev(df$SitePat))
 # Normalize
 s <- sum(df$I)
 df$I <- df$I / s
-df$low <- df$low / s
-df$high <- df$high / s
 
 # Adjust text size. Default is 11
 mytheme = theme_get()
 mytheme$text$size = 20
 theme_set(mytheme)
 
-ggplot(df, aes(SitePat, I, ymin=low, ymax=high)) +
-    geom_pointrange(pch=1, size=1) +
+ggplot(df, aes(SitePat, I, ymin=0, ymax=max(I))) +
+    geom_point(pch=16, size=3) +
     theme(aspect.ratio=14/10,
           axis.title.y = element_blank()) +
     ylab("Site Pattern Frequency") +
     coord_flip()
 
-ggsave("quechua_patfrq.pdf")
+ggsave("quechua_patfrq_rma.pdf")
 
